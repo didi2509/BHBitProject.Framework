a909436deef4d4f62803a9cd8518bb48c07473b5
Diogo de Freitas Nunes <diogon1@hotmail.com>
2017-07-05 23:37:30 -0300

    -> Reparo

---
+++ BBP.BLL/Base/BaseBLL.cs
@@ -0,0 +1,23 @@
+﻿using BBP.DAL.Entity;
+using BBP.DAL.Entity.Interface;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBP.BLL.Entity.Base
+{
+    public class BaseBLL<EntityType> where EntityType : class, IObjectWithKey
+    {
+        public BaseBLL(DbContext _context)
+        {
+            this.Context = _context;
+            DAL = new BaseDAL<EntityType>(_context);
+        }
+
+        protected DbContext Context { get; set; }
+        protected BaseDAL<EntityType> DAL { get; set; }
+    }
+}
